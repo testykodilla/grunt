cbd99b612e6286280a0c3ab0ba20f0b58deb8799
Your Name <you@example.com>
2017-10-11 02:20:45 +0200

    jezu jak zle byl opisany ten modul z gruntem, spedzilem 2h na grzebaniu w necie jak to zrobic, bo ktos nie przejrzal modulu przed scommitowaniem

---
+++ gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    watch: {
+  scripts: {
+    files: './sass/*.sass',
+    tasks: ['default'],
+    options: {
+      spawn:false,
+      event:['all']
+    },
+  },
+},
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        },
+      },
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    }
+
+
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'imagemin']);
+};
